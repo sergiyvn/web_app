b5c852879357009c858668104e92343dca7638bc
Serhiy Nyzhnyk <sergiyvn@gmail.com>
2015-01-12 13:34:55 +0200

    added background image

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module WebShop
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # config.serve_static_assets = true
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     I18n.enforce_available_locales = true
   end
